1560f0f57349d9e749e176a43711a87761b79310
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-06 10:45:21 +0100

    0 fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """renders Hello world"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
