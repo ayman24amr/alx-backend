10bb024d2f5325717f8f47723d33f55bb0bc9083
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-06 19:30:10 +0100

    fix

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -33,7 +33,7 @@ def get_locale():
 @app.route("/", strict_slashes=False)
 def index():
     """renders Hello world"""
-    return render_template('1-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
